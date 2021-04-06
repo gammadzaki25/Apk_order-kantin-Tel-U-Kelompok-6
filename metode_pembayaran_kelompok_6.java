5f367e9f5de10f8c0e2c28f39beaf1d7ec903863
gammadzaki <55172026+gammadzaki@users.noreply.github.com>
2021-04-06 21:21:29 +0700

    Add files via upload

---
+++ metode_pembayaran_kelompok_6.java
@@ -0,0 +1,39 @@
+
+package relasi_kelompok_6;
+
+
+public class metode_pembayaran_kelompok_6 {
+    private String emoney;
+    private String tunai;
+
+    
+    public metode_pembayaran_kelompok_6(String Emoney, string Tunai){
+        
+        emoney = Emoney;
+        tunai = Tunai;
+
+    }
+    
+    public String getEmoney(){
+        
+        return emoney;
+    }
+    
+    public String getTunai(){
+        
+        return tunai;
+    }
+    
+    
+    public void setEmoney(String Emoney){
+        
+        emoney = Emoney;
+    }
+    
+    public void setTunai(String Tunai){
+        
+        tunai = Tunai;
+    }
+    
+   
+}

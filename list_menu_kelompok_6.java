8459413bb4e70d15a7c4cea91775d49b2cb5c371
rizalsyalman <55252589+rizalsyalman@users.noreply.github.com>
2021-06-05 14:58:24 +0700

    Update list_menu_kelompok_6.java

--- list_menu_kelompok_6.java
+++ list_menu_kelompok_6.java
@@ -1,4 +1,3 @@
-
 package entitas_kelompok_6;
 
 
@@ -9,6 +8,7 @@ public class list_menu_kelompok_6 {
     private int jumlah_porsi;
     private String deskripsi_menu;
     
+    //untuk membuat constructornya
     public list_menu_kelompok_6(String nama, int harga, int jum, String deskripsi){
         nama_menu = nama;
         harga_menu = harga;
@@ -16,32 +16,47 @@ public class list_menu_kelompok_6 {
         deskripsi_menu = deskripsi;
     }
     
+    //Untuk membuat setter nama_menu
     public void setNama_Menu(String nama){
         nama_menu = nama;
     }
+    
+    //Untuk membuat setter harga_menu
     public void setHarga_Menu(int harga){
         harga_menu = harga;
     }
+    
+    //Untuk membuat setter jumlah_porsi
     public void setJumlah_Porsi(int jum){
         jumlah_porsi = jum;
     }
+    
+    //Untuk membuat setter deskripsi_menu
     public void setDeskripsi_Menu(String deskripsi){
         deskripsi_menu = deskripsi;
     }
     
+    //Untuk membuat getter nama_menu
     public String getNama_Menu(){
         return nama_menu;
     }
+    
+    //Untuk membuat getter harga_menu
     public int getHarga_Menu(){
         return harga_menu;
     }
+    
+    //untuk membuat getter jumlah_porsi
     public int getJumlah_Porsi(){
         return jumlah_porsi;
     }
+    
+    //untuk membuat getter deskripsi_menu
     public String getDeskripsi_Menu(){
         return deskripsi_menu;
     }
     
+    //Untuk format tampilan List_menu nya
     public void infoListmakanan(list_menu_kelompok_6[] kantin1_menu){
         
         for (int i = 0 ; i < 5 ; i++) {
